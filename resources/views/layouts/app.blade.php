36a93f3e856ed0a56e3f37f01bb4376116be2c94
Ismael <84606443+ismaaadc@users.noreply.github.com>
2022-11-19 00:13:57 +0000

    Adding Flowbite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">

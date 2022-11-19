fd792fbe46766d9a12e3d8c5ab966cb40b1dd82d
Ismael <84606443+ismaaadc@users.noreply.github.com>
2022-11-19 20:14:08 +0000

    Modifying default route

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index'])->name('index');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');

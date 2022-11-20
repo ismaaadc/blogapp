50844a1d51e82261eba4d53ed88161a4d2e16555
Ismael <84606443+ismaaadc@users.noreply.github.com>
2022-11-20 01:49:09 +0000

    Fixing Database Migration

--- database/migrations/2022_11_19_200449_create_categories_table.php
+++ database/migrations/2022_11_19_200449_create_categories_table.php
@@ -15,14 +15,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 255);
+            $table->string('name', 255)->unique();
             $table->timestamps();
         });
-
-        //Adding default record
-        Role::firstOrCreate([
-            'name' => 'unasigned'
-        ]);
         
     }
 
